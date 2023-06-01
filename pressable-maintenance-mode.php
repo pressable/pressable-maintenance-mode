8d6fc117d3fd5ae1c0b2506855b19cf35ec299ab
Wayne McWilliams <waynemcwilliams@Waynes-MBP-2.lan>
2023-06-01 10:22:35 -0500

    Restyled the maintenance mode and removed branding and images

--- pressable-maintenance-mode.php
+++ pressable-maintenance-mode.php
@@ -4,7 +4,7 @@ Plugin Name: Pressable Maintenance Mode
 Plugin URI: https://pressable.com
 Description: A simple maintenance mode plugin
 Author: Pressable
-Version: 1.0.2
+Version: 1.0.3
 Author URI: https://pressable.com/
 License: GPL2
 */
@@ -74,67 +74,82 @@ function wp_maintenance_mode() {
                 border: none;
                 font-size: 3em;
                 color: #ffffff;
+                text-align: center;
             }
 
             .press-maintenance p {
                 font-size: 1.4em !important;
                 margin-top: 10px !important;
                 color: #ffffff;
+                text-align: center;
             }
 
-            .press-maintenance img {
-                max-width: 600px;
-                width: 100%;
-                height: auto;
-            }
-
-            .press-maintenance > div {
-                margin-right: 2em;
-            }
-
-            .press-top-line {
-                background-color: rgba(0,0,0,.4);
-                height: 60px;
-                position: absolute;
-                top: 0;
-                left: 0;
-                width: 100%;
-                z-index: 50;
-                display: flex;
-                justify-content: center;
-            }
-
-            .press-top-line img {
-                width: 100%;
-                max-width: 210px;
-                height: 35px;
-                margin-top: 12px;
-            }
-
-            @keyframes gradient {
-              0% {
-                  background-position: 0% 50%;
+            .spinner {
+                -webkit-animation: rotate 2s linear infinite;
+                        animation: rotate 2s linear infinite;
+                z-index: 2;
+                position: relative;
+                width: 50px;
+                height: 50px;
               }
-
-              50% {
-                  background-position: 100% 50%;
+              .spinner .path {
+                stroke: #93bfec;
+                stroke-linecap: round;
+                -webkit-animation: dash 1.5s ease-in-out infinite;
+                        animation: dash 1.5s ease-in-out infinite;
               }
-
-              100% {
-                  background-position: 0% 50%;
+              
+              @-webkit-keyframes rotate {
+                100% {
+                  transform: rotate(360deg);
+                }
               }
-            }
+              
+              @keyframes rotate {
+                100% {
+                  transform: rotate(360deg);
+                }
+              }
+              @-webkit-keyframes dash {
+                0% {
+                  stroke-dasharray: 1, 150;
+                  stroke-dashoffset: 0;
+                }
+                50% {
+                  stroke-dasharray: 90, 150;
+                  stroke-dashoffset: -35;
+                }
+                100% {
+                  stroke-dasharray: 90, 150;
+                  stroke-dashoffset: -124;
+                }
+              }
+              @keyframes dash {
+                0% {
+                  stroke-dasharray: 1, 150;
+                  stroke-dashoffset: 0;
+                }
+                50% {
+                  stroke-dasharray: 90, 150;
+                  stroke-dashoffset: -35;
+                }
+                100% {
+                  stroke-dasharray: 90, 150;
+                  stroke-dashoffset: -124;
+                }
+              }
         </style>
 
-        <div class="press-top-line">
-            <img src="https://149443700.v2.pressablecdn.com/wp-content/uploads/2021/11/white_logo_Big-10-300x47.png" alt="Pressable - Managed WordPress Hosting" />
-        </div>
         <div class="press-maintenance">
             <div>
                 <h1>We will be right back!</h1>
                 <p>This website is currently running a brief maintenance.</p>
+                <div style="margin: 2em auto;text-align:center;">
+                    <svg class="spinner" viewBox="0 0 50 50">
+                        <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+                    </svg>
+                </div>
             </div>
-            <img src="https://149443700.v2.pressablecdn.com/wp-content/uploads/2022/03/server-stack@2x-300x171.png" alt="Hosting Servers" />
         </div>
       ');
     }
